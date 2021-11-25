f6628c68831e812c4149e719545b8ae93f97300c
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2021-11-24 22:04:07 -0600

    Fix port

--- Connection.js
+++ Connection.js
@@ -11,5 +11,6 @@ app.use('/api', require('./mysql.js'));
 app.use('/api', require('./mongo.js'));
 app.use('/api', require('./redis.js'));
 
-app.listen(3001, () =>
-    console.log("running on port 3001"));
+app.listen(process.env.PORT || 3001, function(){
+    console.log("running on port 3001", this.address().port, app.settings.env)
+});
--- mongo.js
+++ mongo.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
         }
         callback(null, uuidv4() + ext);
       } catch (error) {
-        alert("El archivo selecionado" + error.name + "Solo se admiten archivos tipo: \n .png \n .jpg \n .gif \n .jpeg")
+        console.log("El archivo selecionado" + error.name + "Solo se admiten archivos tipo: \n .png \n .jpg \n .gif \n .jpeg")
       }
     }
 }) 
